5adffbb45d9dfa81ec69f1d5d95e227216e43bb5
joseph-cef <josephdev1962@gmail.com>
2025-11-30 22:53:56 +0100

    ..

--- server.js
+++ server.js
@@ -7,3 +7,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+// هاذ ملف server.js هو الملف الرئيسي لتشغيل تطبيق Express. في البداية، يتم استيراد مكتبة dotenv لتحميل المتغيرات البيئية من ملف config.env. بعد ذلك، يتم استيراد التطبيق من ملف app.js.
